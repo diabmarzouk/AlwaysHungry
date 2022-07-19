d9e683c973344a0f6c3389e5987ec5e4cec2bb52
diabmarzouk <omardiabmarzouk@gmail.com>
2022-07-18 20:24:29 -0700

    Pushing first XML UI file, the welcome screen. Will add functionality to the start button once the next screen is complete.

--- app/src/main/java/com/cmpt362/alwayshungry/MainActivity.kt
+++ app/src/main/java/com/cmpt362/alwayshungry/MainActivity.kt
@@ -2,16 +2,21 @@ package com.cmpt362.alwayshungry
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var textView:TextView
+
+    private lateinit var startBtn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startBtn = findViewById(R.id.startBtn)
 
-        textView = findViewById(R.id.firstTextView)
-        textView.text = "My name is OMAR"
+
+//        startBtn.setOnClickListener()
+
 
     }
 }
