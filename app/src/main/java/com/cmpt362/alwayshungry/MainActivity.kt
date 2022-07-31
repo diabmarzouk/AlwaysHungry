1250a8a6edfc9c695ac0b40e9118f5856eee3835
diabmarzouk <omardiabmarzouk@gmail.com>
2022-07-30 22:37:36 -0700

    Added new activities as activities in Manifest Updated Firebase on the console (website) to allow for email authentication and account creation Updated some code in SignUpActivity.kt to allow for proper account creation.

--- app/src/main/java/com/cmpt362/alwayshungry/MainActivity.kt
+++ app/src/main/java/com/cmpt362/alwayshungry/MainActivity.kt
@@ -7,16 +7,43 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.widget.Button
 import android.widget.TextView
+import com.cmpt362.alwayshungry.auth.SignUpActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var startBtn : Button
+    private lateinit var authObj: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.welcome_screen)
         startBtn = findViewById(R.id.startBtn)
 
+        authObj = Firebase.auth
+
+        val currUser = authObj.currentUser
+
+        // If there is no user already logged in from the apps memory...
+        if(currUser == null){
+
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+
+            // Launch intent to launch SignUpActivity
+            //  This activity will have a sign UP screen to begin with and if
+            //      the user already has an account, they can click a button that will
+            //      launch another intent to launc the LoginActivity
+
+
+
+        }else{
+            // Launch intent to go straight to the fragment tab page
+            val intent = Intent(this, smartFridgeManager::class.java)
+            startActivity(intent)
+        }
 
         startBtn.setOnClickListener{
             val intent = Intent(this, userInfo::class.java )
--- app/src/main/java/com/cmpt362/alwayshungry/auth/SignUpActivity.kt
+++ app/src/main/java/com/cmpt362/alwayshungry/auth/SignUpActivity.kt
@@ -1,10 +1,14 @@
 package com.cmpt362.alwayshungry.auth
 
 import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.cmpt362.alwayshungry.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -14,11 +18,38 @@ class SignUpActivity : AppCompatActivity() {
     // create Firebase authentication object
     private lateinit var authObj: FirebaseAuth
 
+    //XML layout variables
+    private lateinit var emailET: EditText
+    private lateinit var passwordET: EditText
+    private lateinit var signUpBtn : Button
+    private lateinit var loginBtn: Button
+
+    //Variables
+    private lateinit var email : String
+    private lateinit var password: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.signup_activity)
+        emailET = findViewById(R.id.signUpEmail)
+        passwordET = findViewById(R.id.signUpPassword)
+        signUpBtn = findViewById(R.id.signUpContinueBtn)
+        loginBtn = findViewById(R.id.signUpLoginBtn)
 
         //Initialize auth object
         authObj = Firebase.auth
+
+        loginBtn.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        signUpBtn.setOnClickListener {
+            email = emailET.text.toString()
+            password = passwordET.text.toString()
+            createAccount(email, password)
+
+        }
 
         //... Here we create an activity that will be a screen that allows the user
         //      to sign up and we take this data, shove it into our Firebase using
@@ -29,12 +60,14 @@ class SignUpActivity : AppCompatActivity() {
     public override fun onStart() {
         super.onStart()
 
-        val currUser = authObj.currentUser
-        if(currUser != null){
+
+        // No need to check if currUser is null because we already did this in MainActivity
+//        val currUser = authObj.currentUser
+//        if(currUser != null){
 
             // Reload function will reload the activity so that the user gets send to the login screen
 //            reload()
-        }
+//        }
 
     }
 
