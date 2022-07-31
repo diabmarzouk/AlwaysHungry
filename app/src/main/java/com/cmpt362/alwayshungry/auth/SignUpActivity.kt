7b9700d77922abb3af28cc8aa44ec8671f5d9fe1
diabmarzouk <omardiabmarzouk@gmail.com>
2022-07-30 20:40:47 -0700

    Moved google-services.json to app level in the folder architecture Added some basic test code in SignUpActivity.kt

--- app/src/main/java/com/cmpt362/alwayshungry/auth/SignUpActivity.kt
+++ app/src/main/java/com/cmpt362/alwayshungry/auth/SignUpActivity.kt
@@ -1,6 +1,9 @@
 package com.cmpt362.alwayshungry.auth
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -9,18 +12,70 @@ import com.google.firebase.ktx.Firebase
 class SignUpActivity : AppCompatActivity() {
 
     // create Firebase authentication object
-    private lateinit var auth: FirebaseAuth
+    private lateinit var authObj: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         //Initialize auth object
-        auth = Firebase.auth
+        authObj = Firebase.auth
 
         //... Here we create an activity that will be a screen that allows the user
         //      to sign up and we take this data, shove it into our Firebase using
         //      Firestore database...
 
+    }
+
+    public override fun onStart() {
+        super.onStart()
+
+        val currUser = authObj.currentUser
+        if(currUser != null){
+
+            // Reload function will reload the activity so that the user gets send to the login screen
+//            reload()
+        }
+
+    }
+
+    private fun createAccount(email: String, password: String){
+        authObj.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = authObj.currentUser
+
+                   // updateUI is a function that might be implemented later
+//                    updateUI(user)
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+//                    updateUI(null)
+                }
+            }
+    }
+
+    private fun signIn(email: String, password: String){
+        authObj.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = authObj.currentUser
+
+//                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+//                    updateUI(null)
+                }
+            }
     }
 
 }
